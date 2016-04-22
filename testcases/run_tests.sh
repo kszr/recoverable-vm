ce67d42d49987bd484c57fe4d69d69aaa705109a
kszr <abhishek.rob@gmail.com>
2016-04-21 23:28:28 -0400

    Adding tests and a script to compile and run all tests

---
+++ testcases/run_tests.sh
@@ -0,0 +1,45 @@
+# Compile all tests
+echo "Compiling basic.c";
+g++ -std=c++11 basic.c ../rvm.cpp -o basic;
+echo "Compiling multi.c";
+g++ -std=c++11 multi.c ../rvm.cpp -o multi;
+echo "Compiling abort.c";
+g++ -std=c++11 abort.c ../rvm.cpp -o abort;
+echo "Compiling multi-abort.c";
+g++ -std=c++11 multi-abort.c ../rvm.cpp -o multi-abort;
+echo "Compiling truncate.c";
+g++ -std=c++11 truncate.c ../rvm.cpp -o truncate;
+echo "Compiling test_deception.c";
+g++ -std=c++11 test_deception.c ../rvm.cpp -o test_deception;
+echo "Compiling out_of_order.c";
+g++ -std=c++11 out_of_order.c ../rvm.cpp -o out_of_order;
+echo ""
+
+# Run all tests
+echo "======= Running basic.c =======";
+echo"";
+./basic;
+echo "";
+echo "======= Running multi.c =======";
+echo "";
+./multi;
+echo "";
+echo "======= Running abort.c =======";
+echo "";
+./abort;
+echo "";
+echo "======= Running multi-abort.c =======";
+echo "";
+./multi-abort;
+echo "";
+echo "======= Running truncate.c =======";
+echo "";
+./truncate;
+echo "";
+echo "======= Running test_deception.c =======";
+echo "";
+./test_deception;
+echo "";
+echo "======= Running out_of_order.c =======";
+echo "";
+./out_of_order;
